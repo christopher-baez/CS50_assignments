929238dc6c5d9db6936054e4b50b109e1da9bfef
christopher.baez <christopherbaez8@gmail.com>
2023-10-31 14:51:16 -0500

    renamed the file

---
+++ 2/camel.py
@@ -0,0 +1,17 @@
+def main():
+    # get input name
+    names = input("camelCase: ")
+
+# loop through each letter
+    for name in names:
+        if
+
+#  check if upper case
+
+
+# print underscore and letter lower case
+
+
+# otherwise print letter
+
+# print space in the end
