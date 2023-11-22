6de25607ff7ceafb8ed827599358688f1dd2f39e
christopher.baez <christopherbaez8@gmail.com>
2023-11-22 17:41:32 -0600

    completed exercise for CS50

--- 4/emojize.py
+++ 4/emojize.py
@@ -1,9 +1,11 @@
+# importing emoji library
 import emoji
 
 def main():
-
+    # asking for input
     emo = input('Input: ')
 
+    # printing input as an emoji and added alias
     print(emoji.emojize(f'Output:{emo}', language='alias'))
 
 main()
